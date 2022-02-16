c8e0de69ed715e7df0c7bf03cbcef9bbd465804f
Nokhalal Mahato <nokhalal412000@gmail.com>
2022-02-16 12:07:55 +0530

    code

--- Linked List/Put_even_posistion_node_after_odd.cpp
+++ Linked List/Put_even_posistion_node_after_odd.cpp
@@ -24,11 +24,7 @@ void insertion(node* &head,int n){
     }    
 }
 
-void insertion_head(node* &head,int n){
-    node* el=new node(n);
-    el->next=head;
-    head=el;
-}
+
 void display(node* head){
     while(head!=NULL){
         cout<<head->data<<"->";
@@ -36,12 +32,29 @@ void display(node* head){
     }
     cout<<"NULL";
 }
+void change_order(node* &head){
+    if(head==NULL || head->next==NULL)
+        return;
+    node* evenptr=head->next; 
+    node* even=evenptr;   
+    node* oddptr=head;
+    while(evenptr->next!=NULL && oddptr->next!=NULL){
+        oddptr->next=evenptr->next;
+        oddptr=oddptr->next;
+        evenptr->next=oddptr->next;
+        evenptr=evenptr->next;
+    }
+    if(oddptr->next==NULL)
+        evenptr->next=NULL;
+    oddptr->next=even;    
+}
 
 int main(){
     node* head=NULL;
-    insertion(head,1);
-    insertion(head,2);
-    insertion(head,3);
-    insertion_head(head,4);
+    int a[]={1,2,3,4,5,6};
+    for(int i=0;i<6;i++){
+        insertion(head,a[i]);
+    }
+    change_order(head);
     display(head);
 }
