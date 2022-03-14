d960b144f9c3e246955b4a1151f691cdd5e9fb47
Nokhalal Mahato <nokhalal412000@gmail.com>
2022-03-14 11:36:38 +0530

    graph

---
+++ Graph/adjacency_list.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int> adj[1000];
+    for(int i=0;i<m;i++){
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    for(int i=1;i<n+1;i++){
+        cout<<i<<"->";
+        for(int j=0;j<adj[i].size();j++){
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
